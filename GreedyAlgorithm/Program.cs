83593b3154d716f8bfa6fe37b7a1fc9247f5dd9a
Daniel Cirket <daniel@atlascode.com>
2017-10-20 16:57:12 +0100

    Add project files.

---
+++ GreedyAlgorithm/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GreedyAlgorithm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
